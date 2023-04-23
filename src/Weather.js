e017393a9061cdbeaefe36077aed13e8cb4b7fbc
Yoon Me Me Phyo <yoommeemeephyo@gmail.com>
2023-04-23 12:35:33 +0630

    Adding props

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
 
     function handleResponse(response) {
-        alert(`The weather in Sydney is ${response.data.main.temp}`);
+        alert(`The weather in ${props.city} is ${response.data.main.temp}°C`);
     }
 
     let apiKey = "0dc40d3d7cda209ca40e77430c74cf57";
 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-
 
     return (
         <h2>Weather App</h2>
